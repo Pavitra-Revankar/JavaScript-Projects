3cd517bdb33f3434b05be79eaea4450438f83361
Pavitra Revankar <g.pavitra7@gmail.com>
2023-07-18 18:40:15 +0530

    Update index.js

--- Passenger Count/index.js
+++ Passenger Count/index.js
@@ -1,25 +1,27 @@
-// let count = 0
-// let btn_ele=document.getElementById("count-el")
-// let prev=document.getElementById("previous")
+ let count = 0
+ let btn_ele=document.getElementById("count-el")
+ let prev=document.getElementById("previous")
 
-// function increment(){
-//     count = count + 1
-//     console.log(count)
+ function increment(){
+     count = count + 1
+     console.log(count)
     
-//     btn_ele.innerText = count
-// }
+     btn_ele.innerText = count
+ }
 
-// function save(){
-//     console.log(count)
-// // the below code does not displays the right space of " - "
-// // so change it to "textContent" 
-//     // prev.innerText += count  + " - "
-//     prev.textContent += count  + " - "
-//     btn_ele.innerText = 0
-//     count = 0
-// }
+ function save(){
+     console.log(count)
+ // the below code does not displays the right space of " - "
+ // so change it to "textContent" 
+     // prev.innerText += count  + " - "
+     prev.textContent += count  + " - "
+     btn_ele.innerText = 0
+     count = 0
+ }
 
-let num1 = 8
+
+// Code for simple calculator
+/*let num1 = 8
 let num2 = 2
 
 document.getElementById("num1-el").textContent = num1
@@ -45,4 +47,4 @@ function divide(){
 function multiplication(){
     let sum = num1 * num2
     Sum.textContent = "Sum: " + sum
-}
+}*/
